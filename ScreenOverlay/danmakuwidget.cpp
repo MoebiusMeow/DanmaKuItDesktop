533befc4b005f2ae668c21e7e103199c43d715d1
dcy11011 <dcy11011@foxmail.com>
2021-11-30 21:26:51 +0800

    Add websocket to screenoverlay

--- ScreenOverlay/danmakuwidget.cpp
+++ ScreenOverlay/danmakuwidget.cpp
@@ -11,47 +11,63 @@ DanmakuWidget::DanmakuWidget(QWidget *parent) : QWidget(parent)
     , m_textTopSet(new DanmakuTextTopSet)
     , m_textBottomSet(new DanmakuTextBottomSet)
     , update_timer(new QTimer)
-    , paint_timer(new QTimer)
+    , reconnect_timer(new QTimer)
     , m_test(new DanmakuTestSet)
+    , m_websocket(new QWebSocket)
 {
+    // config websocket
+    m_url = "ws://127.0.0.1:8000";
+    connect(m_websocket, &QWebSocket::connected, this, &DanmakuWidget::onconnect);
+    connect(m_websocket, &QWebSocket::disconnected, this, &DanmakuWidget::ondisconnect);
+    connect(m_websocket, &QWebSocket::binaryMessageReceived, this, &DanmakuWidget::onBinaryMessageRecieved);
+    connect(m_websocket, &QWebSocket::textMessageReceived, this, &DanmakuWidget::onTextMessageReceived);
+    m_websocket->open(m_url);
+    qDebug()<<"conncting...";
+
+    // config update_timer
     connect(update_timer, &QTimer::timeout, this, &DanmakuWidget::updateText);
     update_timer->start(UPDATE_INTERVAL);
 }
 
 DanmakuWidget::~DanmakuWidget()
 {
+    m_websocket->close();
     delete m_textFloatSet;
     delete m_textTopSet;
     delete m_textBottomSet;
     delete update_timer;
-    delete paint_timer;
+    delete reconnect_timer;
     delete m_test;
+    delete m_websocket;
 }
 
-int DanmakuWidget::append(const QString &text, const QColor &color)
+int DanmakuWidget::appendFloat(const QString &text, const QString &id, const QColor &color, int size)
 {
     std::shared_ptr<DanmakuTextFloat> newText = std::make_shared<DanmakuTextFloat>();
     newText->setText(text);
     newText->setColor(color);
+    newText->setFontSize(size);
     m_textFloatSet->append(newText);
     //m_waiting.push_back(newText);
     return true;
 }
 
-int DanmakuWidget::appendTop(const QString &text, const QColor &color)
+int DanmakuWidget::appendTop(const QString &text, const QString &id, const QColor &color, int size)
 {
     std::shared_ptr<DanmakuTextTop> newText = std::make_shared<DanmakuTextTop>();
     newText->setText(text);
     newText->setColor(color);
+    newText->setFontSize(size);
     m_textTopSet->append(newText);
     return true;
 }
 
-int DanmakuWidget::appendBottom(const QString &text, const QColor &color)
+int DanmakuWidget::appendBottom(const QString &text, const QString &id, const QColor &color, int size)
 {
     std::shared_ptr<DanmakuTextBottom> newText = std::make_shared<DanmakuTextBottom>();
     newText->setText(text);
     newText->setColor(color);
+    newText->setFontSize(size);
     m_textBottomSet->append(newText);
     return true;
 }
@@ -77,8 +93,6 @@ void DanmakuWidget::paintEvent(QPaintEvent *paint_event)
     m_textFloatSet->paint(&p);
     m_textTopSet->paint(&p);
     m_textBottomSet->paint(&p);
-    p.setBrush(Qt::blue);
-    p.drawEllipse(QPoint(width()/2, height() * 0.6),50,50);
     raise();
     Q_UNUSED(paint_event);
 }
@@ -91,16 +105,71 @@ void DanmakuWidget::resizeEvent(QResizeEvent *resize_event)
     Q_UNUSED(resize_event);
 }
 
-void DanmakuWidget::mousePressEvent(QMouseEvent *mouse_event)
+void DanmakuWidget::onconnect()
 {
-    if(mouse_event->button()==Qt::LeftButton){
-        append(m_test->getRandonStr());
+    qDebug()<<"connnected to room server";
+}
+
+void DanmakuWidget::ondisconnect()
+{
+    //reconnect_timer->singleShot(DANMAKU_RECONNECT_INTERVAL,this,&DanmakuWidget::reconnect);
+}
+
+void DanmakuWidget::reconnect()
+{
+    m_websocket->abort();
+    m_websocket->open(m_url);
+}
+
+void DanmakuWidget::onBinaryMessageRecieved(const QByteArray &message)
+{
+    //
+    QString str_message;
+    str_message.prepend(message);
+    str_message.replace('\'','\"');
+    str_message.remove('\t');
+    QByteArray processed_message = str_message.toUtf8();
+    QJsonParseError error;
+    QJsonDocument doc = QJsonDocument::fromJson(processed_message, &error);
+    if(!doc.isNull()){
+        QJsonObject json_obj = doc.object();
+        QString message_type = json_obj.value("payload").toString();
+
+        QJsonObject properties = json_obj.value("properties").toObject();
+        if(properties.value("permission").toString().toInt()==DANMAKU_PERMISSION_ALLOW){
+            QString content = properties.value("content").toString();
+            QString id      = properties.value("id").toString();
+            QString color_name = properties.value("color").toString();
+            int     size    = properties.value("size").toString().split("pt", Qt::SkipEmptyParts)[0].toInt();
+            QString pos     = properties.value("pos").toString();
+            qDebug() << "content="<<content<<"  id="<<id<<" color="<<color_name<<"  size="<<size<<"  pos="<<pos;
+
+            QColor color = QColor(0xFFFFFF);
+            if(color_name != "undefined") color = QColor(color_name.toInt(NULL, 16));
+            if(pos == "rightleft"){
+                appendFloat(content, id, color, size);
+            }
+            if(pos == "top"){
+                appendTop(content, id, color, size);
+            }
+            if(pos == "bottom"){
+                appendBottom(content, id, color, size);
+            }
+        }
+        else if(message_type == "AAAB"){
+            QString id      = properties.value("id").toString();
+            std::shared_ptr<DanmakuText> p_text = nullptr;
+            if(p_text == nullptr) p_text = m_textBottomSet->findByID(id);
+            if(p_text == nullptr) p_text = m_textFloatSet->findByID(id);
+            if(p_text == nullptr) p_text = m_textTopSet->findByID(id);
+
+            if(p_text != nullptr) p_text->del();
+        }
+
     }
-    if(mouse_event->button()==Qt::RightButton){
-        appendTop(m_test->getRandonStr());
-    }
-    if(mouse_event->button()==Qt::MiddleButton){
-        appendBottom(m_test->getRandonStr());
-    }
-    Q_UNUSED(mouse_event)
+}
+
+void DanmakuWidget::onTextMessageReceived(const QString &message)
+{
+    qDebug()<<"[Text]"<<message;
 }
--- ScreenOverlay/danmakuwidget.h
+++ ScreenOverlay/danmakuwidget.h
@@ -3,6 +3,9 @@
 
 #include <QWidget>
 #include <QTimer>
+#include <QWebSocket>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include "danmakutextfloatset.h"
 #include "danmakutexttopset.h"
 #include "danmakutextbottomset.h"
@@ -19,23 +22,30 @@ public:
     const int UPDATE_INTERVAL = 1000/DANMAKU_FPS;
     const int PAINT_INTERVAL  = 1000/DANMAKU_FPS;
 
-    int append(const QString &text, const QColor &color = Qt::cyan);
-    int appendTop(const QString &text, const QColor &color = Qt::magenta);
-    int appendBottom(const QString &text, const QColor &color = Qt::yellow);
+    int appendFloat(const QString &text, const QColor &color = Qt::cyan, int size=20);
+    int appendTop(const QString &text, const QColor &color = Qt::magenta, int size=20);
+    int appendBottom(const QString &text, const QColor &color = Qt::yellow, int size=20);
 
-protected slots:
+protected Q_SLOTS:
     bool updateText();
     bool updateDisplay();
     void paintEvent(QPaintEvent *paint_event) override;
     void resizeEvent(QResizeEvent *resize_event) override;
-    void mousePressEvent(QMouseEvent *mouse_event) override;
+
+    void onconnect();
+    void ondisconnect();
+    void reconnect();
+    void onBinaryMessageRecieved(const QByteArray &message);
+    void onTextMessageReceived(const QString &message);
 
 private:
     DanmakuTextFloatSet *m_textFloatSet;
     DanmakuTextTopSet *m_textTopSet;
     DanmakuTextBottomSet *m_textBottomSet;
-    QTimer *update_timer, *paint_timer;
+    QTimer *update_timer, *reconnect_timer;
     DanmakuTestSet *m_test;
+    QWebSocket *m_websocket;
+    QString m_url;
 };
 
 #endif // DANMAKUWIDGET_H
